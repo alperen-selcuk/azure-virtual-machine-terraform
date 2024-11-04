2c60f1b637c1e6eeefa156b49da114980edcbbd8
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2024-11-04 15:27:43 +0300

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "test" {
   name                = var.name.vnet
-  location            = azurerm_resource_group.kubespray.location
-  resource_group_name = azurerm_resource_group.kubespray.name
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
   address_space       = [var.cidr]
 
   tags = {
